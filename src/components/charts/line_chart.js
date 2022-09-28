6ecbd52e2d187c4fac8f25e462644f2942686991
tushar725mittal <tushar725mittal@gmail.com>
2022-09-29 04:10:18 +0530

    04:10

---
+++ src/components/charts/line_chart.js
@@ -0,0 +1,9 @@
+import { ChartComponent } from '@syncfusion/ej2-react-charts';
+import * as React from 'react';
+
+class LineChart extends React.Component {
+    render() {
+        return (<ChartComponent />);
+    }
+}
+export default LineChart;
