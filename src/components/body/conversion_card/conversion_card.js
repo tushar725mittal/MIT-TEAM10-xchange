bd5b9b744aaa4984db4c53e92068a9a76553de42
Akshit Mahaur <mahaurakshit7@gmail.com>
2022-09-29 19:35:22 +0530

    19:35

--- src/components/body/conversion_card/conversion_card.js
+++ src/components/body/conversion_card/conversion_card.js
@@ -1,24 +1,78 @@
-import React, { Component } from 'react'
-import './conversion_card.css'
+import React, { Component, useEffect } from "react";
+import "./conversion_card.css";
+// import Records from './services/json_fetch/currencies_final.json'
 
+function setCurrencyValue() {
+  document.getElementById("currency-to-value").value =
+    (document.getElementById("currency-from-value").value *
+      document.getElementById("currency-from").value) /
+    document.getElementById("currency-to").value;
+}
+
+function fetchCurrency({}) {
+  var currencies = [];
+  var currency_json = {}
+
+  return (
+    <>
+    <option value={currency_json}> </option>
+    </>
+  )
+
+}
 export default function ConversionCard() {
-    return (
-      <div className='card-container' align="center">
-        <div className='text-container-3'>
-          <h3>CONVERT IT!</h3>
-        </div>
-        <div className='text-container-1'>
-          <input className='input-field' type="number" placeholder='FROM' />
-          <select className='currency-selector'>
-            <option>USD</option>
-          </select>
-        </div>
-        <div className='text-container-2'>
-          <input className='input-field' type="number" placeholder='TO' />
-          <select className='currency-selector'>
-            <option>USD</option>
-          </select>
-        </div>
+  const [currency_json, setCurrencyJson] = React.useState({});
+  useEffect(() => {
+    fetch("currencies_final.json").then((json) => {
+      json.json().then((data) => {
+        setCurrencyJson(data);
+        // console.log(data);
+
+      });
+    });
+  }, []);
+
+  // input.addEventListener('input', setCurrencyValue);
+  return (
+    <div className="card-container" align="center">
+      <div className="text-container-3">
+        <h3>CONVERT IT!</h3>
       </div>
-    );
+      <div className="text-container-1">
+        <input
+          className="input-field"
+          id="currency-from-value"
+          type="number"
+          placeholder="FROM"
+        />
+        <select className="currency-selector">
+          {/* {
+            () => {
+              var l = $("#currency-selector");
+              $.each
+                var currency = currency_json[key]
+                l += <option value={currency['DZD']}>{currency.name}</option>
+
+              } 
+              // console.log(l)  
+              // return l;
+              } */}
+            
+          
+        </select>
+      </div>
+      <div className="text-container-2">
+        <input
+          className="input-field"
+          type="number"
+          id="currency-to-value"
+          disabled
+          placeholder="TO"
+        />
+        <select className="currency-selector">
+          <option>USD</option>
+        </select>
+      </div>
+    </div>
+  );
 }
