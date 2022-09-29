cb8f01c48ecc2cb68eb9200bfc7b9ce8fca5fca5
tushar725mittal <tushar725mittal@gmail.com>
2022-09-29 14:03:50 +0530

    02:03

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ConversionCard from './conversion_card/conversion_card';
 import LineChart from './charts/line_chart';
+import './body.css';
 
 class Body extends React.Component {
     render() {
         return (
             <div className='body'>
-                <div>
-                    <LineChart />
-                </div>
-                <div>
-                    <ConversionCard />
-                </div>
-            </div>
+                <LineChart />
+                <ConversionCard />
+            </div >
         )
     }
 }
