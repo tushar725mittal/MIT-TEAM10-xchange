c38542d0f16bb7c7c43851e84fe3ce2ebb1f31e3
Akshit Mahaur <mahaurakshit7@gmail.com>
2022-09-29 13:49:25 +0530

    13:49

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ConversionCard from './conversion_card/conversion_card';
 import LineChart from './charts/line_chart';
+import './body.css';
 
 class Body extends React.Component {
     render() {
         return (
             <div className='body'>
-                <div>
+                <div className='div1'>
                     <LineChart />
                 </div>
-                <div>
+                <div  className='div1'>
                     <ConversionCard />
                 </div>
             </div>
