0216d190ec7f40c62d02851ec684e51e559c2ba4
tushar725mittal <tushar725mittal@gmail.com>
2022-09-29 18:25:05 +0530

    06:24

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 import Body from './components/body/body';
 import FilterHeader from './components/filter_header/filter_header';
 function App() {
+  var ft = "";
+
+  var filterType = (data) => {
+    ft = data
+    console.log("used");
+    console.log(ft);
+  }
   return (
     <div>
-      <FilterHeader />
+      <FilterHeader filterType={filterType} />
       <Body />
     </div>
   )
